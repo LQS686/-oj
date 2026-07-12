48fdbcf02eb44482310a03b390b1f1218ba56fb8
逍遥老人 <2834937203@qq.com>
2026-07-12 16:51:40 +0800

    chore: untrack logs and 参考资源 directory

--- 参考资源/Project_LemonLime-0.3.6.2/tests/data/TestContest1/source/user1/aplusb.py
+++
@@ -1,4 +0,0 @@
-for x in range(1000000):
-    pass
-a, b = map(int, input().split())
-print(a + b)
--- 参考资源/Project_LemonLime-0.3.6.2/unix/test/mle_static.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int a[100000000];
-// 400 MiB
-
-int main() {
-	puts("Hello World!");
-	return 0;
-}
